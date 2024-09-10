0887b09b52e71cb09b1334c91e6d88884a6cd9c6
itomacjnr <IJ_TOMAC@YAHOO.COM>
2024-09-10 17:23:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write(
     """
 )
 
-cnx = connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
